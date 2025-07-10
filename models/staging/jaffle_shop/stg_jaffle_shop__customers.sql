7bf839b9d300d0590468a870ddb00dee772966da
Tom Molitor <tommymolitor@gmail.com>
2025-07-10 20:18:36 +0000

    updated to match end of module 4

---
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -0,0 +1,4 @@
+select
+    id as customer_id,
+    name as full_name
+from raw.customers
---
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -0,0 +1,5 @@
+select
+    id as order_id,
+    customer as customer_id,
+    ordered_at as order_date
+from raw.orders
