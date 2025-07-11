43014723cf27998e72be4008343de6ff371a3354
Tom Molitor <tommymolitor@gmail.com>
2025-07-11 17:24:52 +0000

    updated sources

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,12 +1,6 @@
 select
     id as order_id,
-<<<<<<< HEAD
-    customer as customer_id,
-    ordered_at as order_date
-from {{ source('jaffle_shop', 'orders') }}
-=======
     user_id as customer_id,
     order_date,
     status
-from raw.jaffle_shop_orders
->>>>>>> 357b79fdb29a2d28274ecbf30db71894697d008d
+from {{ source('jaffle_shop', 'jaffle_shop_orders') }}
