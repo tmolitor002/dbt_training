2d4bf1dc220f2e0e557246951413ffd4b83c5809
Tom Molitor <tommymolitor@gmail.com>
2025-07-11 17:25:57 +0000

    updated customer names

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -21,8 +21,8 @@ customer_orders as (
 final as (
     select
         customers.customer_id,
-        customers.given_name,
-        customers.surname,
+        customers.first_name,
+        customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
